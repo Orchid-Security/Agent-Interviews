e94e4340c98c6e2a6cb6492f9bc0c067a9626af7
matan-yadgar <matan.y@orchid.security>
2024-09-22 20:06:28 +0300

    fix: remove todo comment

--- Core/Program.cs
+++ Core/Program.cs
@@ -18,18 +18,3 @@ public static class Program
                 webBuilder.UseUrls("http://localhost:6666");
             });
 }
-
-/* todo:
-
-1 - add a function that the intervieww will read and explain what it does in plain words
-2 - add one more controller with other endpoints
-3 - come up with issues that the interviewee will have to solve such as:
-    a - null reference
-    b - not protect non-existing path
-    c - race condition
-    d - inefficiency
-    e - deadlock
-    f - passing more data via socket than possible (see Startup)
-    g - not handling exceptions
-
-*/
